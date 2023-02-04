07b907658e1a726cbbc843846d730217a46989f9
jsonmona <jsonmona@outlook.com>
2023-02-04 16:09:09 +0900

    feat(client): Add fps logging

--- src/server/serve.rs
+++ src/server/serve.rs
@@ -18,8 +18,25 @@ pub async fn serve() -> Result<()> {
         let mut cap = CaptureGdi::new()?;
         resolution_tx.send(cap.resolution()).map_err(|_| anyhow!("capture receiver dropped"))?;
 
+        let mut log_time = Instant::now();
+        let mut accumulated_time = Duration::from_secs(0);
+        let mut frames = 0;
+
         loop {
+            let old_time = Instant::now();
             let img = cap.capture()?.copy_data();
+            let now_time = Instant::now();
+
+            frames += 1;
+            accumulated_time += now_time - old_time;
+            if now_time - log_time > Duration::from_secs(10) {
+                log_time = now_time;
+                let fps = frames as f64 / accumulated_time.as_secs_f64();
+                println!("Capture FPS={fps:.2}");
+                frames = 0;
+                accumulated_time = Duration::from_secs(0);
+            }
+
             if let Err(_) = image_tx.blocking_send(img) {
                 break;
             }
