8bd2c5535a8d1b65dd541b5afc0de412de175864
jsonmona <jsonmona@outlook.com>
2023-03-30 18:44:33 +0900

    Fix debug binary

--- src/bin/debug.rs
+++ src/bin/debug.rs
@@ -1,7 +1,25 @@
-#[tokio::main]
-async fn main() {
+use tokio::runtime::Runtime;
+use tokio::task::LocalSet;
+
+fn main() {
     twilight::platform::win32::init_dpi();
     env_logger::init();
 
-    todo!();
+    let runtime = Runtime::new().expect("starting tokio runtime");
+    let rt = runtime.handle().clone();
+
+    std::thread::spawn(move || {
+        let local = LocalSet::new();
+
+        local.spawn_local(async { twilight::server::serve().await.expect("launching server") });
+        rt.block_on(local);
+    });
+
+    let rt = runtime.handle().clone();
+
+    twilight::viewer::launch(
+        rt,
+        "127.0.0.1".parse().expect("valid localhost address"),
+        6497,
+    );
 }
