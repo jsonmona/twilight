a5080ca5ba751e1dcc90ef79cc9c1c3870d8268f
jsonmona <jsonmona@outlook.com>
2023-10-23 19:35:49 +0900

    Move over to pure Rust JPEG libraries

--- src/video/decoder/jpeg.rs
+++ src/video/decoder/jpeg.rs
@@ -1,56 +1,52 @@
-use crate::image::{ColorFormat, ImageBuf};
+use crate::image::{ColorFormat, Image, ImageBuf};
 use crate::util::AsUsize;
 use crate::video::decoder::DecoderStage;
 use anyhow::{ensure, Result};
-use turbojpeg::{Decompressor, PixelFormat};
+use zune_jpeg::JpegDecoder as JDecoder;
+use zune_jpeg::zune_core::colorspace::ColorSpace;
+use zune_jpeg::zune_core::options::DecoderOptions;
 
 #[derive(Debug)]
 pub struct JpegDecoder {
-    width: u32,
-    height: u32,
-    decompressor: Decompressor,
+    width: u16,
+    height: u16,
 }
 
 impl JpegDecoder {
     pub fn new(w: u32, h: u32) -> Result<Self> {
-        let decompressor = Decompressor::new()?;
+        ensure!(w <= u16::MAX as u32 && h <= u16::MAX as u32, "image dimension cannot be larger than 65535");
 
         Ok(JpegDecoder {
-            width: w,
-            height: h,
-            decompressor,
+            width: w as u16,
+            height: h as u16,
         })
     }
 }
 
 impl DecoderStage for JpegDecoder {
     fn resolution(&self) -> (u32, u32) {
-        (self.width, self.height)
+        (self.width as u32, self.height as u32)
     }
 
     fn decode(&mut self, data: &[u8]) -> Result<ImageBuf> {
-        let header = self.decompressor.read_header(data)?;
+        let opts = DecoderOptions::new_fast().jpeg_set_out_colorspace(ColorSpace::BGRA);
+        let mut decoder = JDecoder::new_with_options(opts, data);
+
+        decoder.decode_headers()?;
+
+        let (w, h) = decoder.dimensions().expect("headers already decoded");
 
         ensure!(
-            self.width.equals_usize(header.width) && self.height.equals_usize(header.height),
+            self.width == w && self.height == h,
             "image resolution changed from {}x{} to {}x{}",
             self.width,
             self.height,
-            header.width,
-            header.height
+            w,
+            h
         );
 
-        let mut img = ImageBuf::alloc(self.width, self.height, None, ColorFormat::Bgra8888);
+        let img = decoder.decode()?;
 
-        let image = turbojpeg::Image {
-            pixels: img.data.as_mut_slice(),
-            width: header.width,
-            pitch: img.stride.as_usize(),
-            height: header.height,
-            format: PixelFormat::BGRA,
-        };
-
-        self.decompressor.decompress(data, image)?;
-        Ok(img)
+        Ok(Image::new(self.width as u32, self.height as u32, self.width as u32 * 4, ColorFormat::Bgra8888, img))
     }
 }
--- src/video/encoder/jpeg.rs
+++ src/video/encoder/jpeg.rs
@@ -1,42 +1,45 @@
+use std::io::Cursor;
 use crate::image::{ColorFormat, Image};
 use crate::util::AsUsize;
 use crate::video::encoder::stage::EncoderStage;
 use anyhow::{ensure, Context, Result};
-use turbojpeg::{Compressor, PixelFormat, Subsamp};
+use jpeg_encoder::{ColorType, Encoder, SamplingFactor};
 
 #[derive(Debug)]
 pub struct JpegEncoder {
-    width: u32,
-    height: u32,
-    compressor: Compressor,
+    width: u16,
+    height: u16,
+    yuv444: bool,
 }
 
 impl JpegEncoder {
     pub fn new(w: u32, h: u32, yuv444: bool) -> Result<Self> {
-        let mut compressor = Compressor::new()?;
-        compressor.set_quality(100);
-        if yuv444 {
-            compressor.set_subsamp(Subsamp::None);
-        } else {
-            compressor.set_subsamp(Subsamp::Sub2x2);
-        }
+        ensure!(w <= u16::MAX as u32 && h <= u16::MAX as u32, "image dimension cannot be larger than 65535");
 
         Ok(JpegEncoder {
-            width: w,
-            height: h,
-            compressor,
+            width: w as u16,
+            height: h as u16,
+            yuv444,
         })
     }
 }
 
 impl EncoderStage for JpegEncoder {
     fn resolution(&self) -> (u32, u32) {
-        (self.width, self.height)
+        (self.width as u32, self.height as u32)
     }
 
     fn encode(&mut self, img: Image<&[u8]>) -> Result<Vec<u8>> {
+        let buf_len = max_buffer_size(self.width, self.height).context("image too large")?;
+        let buf = vec![0u8; buf_len];
+        let mut cursor = Cursor::new(buf);
+
+        let mut encoder = Encoder::new(&mut cursor, 90);
+
+        encoder.set_sampling_factor(if self.yuv444 { SamplingFactor::R_4_4_4 } else { SamplingFactor::R_4_2_0 });
+
         ensure!(
-            self.width == img.width && self.height == img.height,
+            self.width as u32 == img.width && self.height as u32 == img.height,
             "image resolution changed from {}x{} to {}x{}",
             self.width,
             self.height,
@@ -44,33 +47,30 @@ impl EncoderStage for JpegEncoder {
             img.height
         );
 
-        let w = self.width.as_usize();
-        let h = self.height.as_usize();
+        let color_type = color_format_to_color_type(img.color_format).context("unknown color_format")?;
 
-        let image = turbojpeg::Image {
-            pixels: img.data,
-            width: w,
-            pitch: img.stride.as_usize(),
-            height: h,
-            format: color_format_to_pixel_format(img.color_format)
-                .with_context(|| format!("unknown color format {:?}", img.color_format))?,
-        };
+        encoder.encode(img.data, self.width, self.height, color_type)?;
 
-        let max_len = self.compressor.buf_len(w, h)?;
-        let mut buf = vec![0; max_len];
-
-        let actual_len = self.compressor.compress_to_slice(image, &mut buf)?;
-        buf.truncate(actual_len);
+        let output_len = cursor.position() as usize;
+        let mut buf = cursor.into_inner();
+        buf.truncate(output_len);
 
         Ok(buf)
     }
 }
 
-fn color_format_to_pixel_format(fmt: ColorFormat) -> Option<PixelFormat> {
+fn color_format_to_color_type(fmt: ColorFormat) -> Option<ColorType> {
     match fmt {
-        ColorFormat::Bgra8888 => Some(PixelFormat::BGRA),
-        ColorFormat::Rgba8888 => Some(PixelFormat::RGBA),
-        ColorFormat::Rgb24 => Some(PixelFormat::RGB),
+        ColorFormat::Bgra8888 => Some(ColorType::Bgra),
+        ColorFormat::Rgba8888 => Some(ColorType::Rgba),
+        ColorFormat::Rgb24 => Some(ColorType::Rgb),
         _ => None,
     }
+}
+
+fn max_buffer_size(width: u16, height: u16) -> Option<usize> {
+    let padded_w = (width as usize).checked_next_multiple_of(16)?;
+    let padded_h = (height as usize).checked_next_multiple_of(16)?;
+
+    padded_w.checked_mul(padded_h)?.checked_mul(6)?.checked_add(2048)
 }
